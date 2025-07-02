bc9478ecdc452f6c8ce21962ed39e5f94f6b9a15
Mohammad Hosein <e.mohamadhosein@gmail.com>
2025-07-02 14:18:02 +0330

    creating user dto

--- src/Users/users.module.ts
+++ src/Users/users.module.ts
@@ -5,11 +5,13 @@ import { JwtModule } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config/dist/config.service";
 import { TypeConfigService } from "src/config/type-config.service";
 import { AuthConfig } from "src/config/auth.config";
+import { ConfigModule } from "@nestjs/config/dist/config.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([User]),
         JwtModule.registerAsync({
+            imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: (config: TypeConfigService) => ({
                 secret: config.get<AuthConfig>('auth')?.jwt.secret,

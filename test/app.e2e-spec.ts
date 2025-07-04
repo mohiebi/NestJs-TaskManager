0c4ed15ae581218bc00b9f7e2e96a2e0b0ed5fc2
Mohammad Hosein <e.mohamadhosein@gmail.com>
2025-07-04 19:57:45 +0330

    add test for users to have roles

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,11 @@
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { TestSetup } from './utils/test-setup';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './../src/Users/user.entity';
+import { Role } from './../src/Users/role.enum';
+import { PasswordService } from './../src/Users/password/password.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AppController (e2e)', () => {
     let testSetup: TestSetup;
@@ -17,21 +22,52 @@ describe('AppController (e2e)', () => {
         await testSetup.teardown();
     });
 
-    const TestUser = {
+    const testUser = {
         email: 'test@example.com',
         name: 'Test User',
         password: 'test-password',
     };
 
+    it('should require authentication for protected routes', async () => {
+        const response = await request(testSetup.app.getHttpServer())
+            .get('/tasks')
+            .expect(401);
+
+        expect(response.body.message).toBe('Unauthorized');
+    });
+
+    it('should allow access to public routes', async () => {
+        const userRepo = testSetup.app.get(getRepositoryToken(User));
+
+        await userRepo.save({
+            ...testUser,
+            roles: [Role.ADMIN],
+            password: await testSetup.app
+                .get(PasswordService)
+                .hashPassword(testUser.password),
+        });
+
+        const response = await request(testSetup.app.getHttpServer())
+            .post('/auth/login')
+            .send({ email: testUser.email, password: testUser.password });
+
+        const decoded = testSetup.app
+            .get(JwtService)
+            .verify(response.body.accessToken);
+
+        expect(decoded.roles).toBeDefined();
+        expect(decoded.roles).toContain(Role.ADMIN);
+    });
+
     it('testing registration (POST)', () => {
         return request(testSetup.app.getHttpServer())
             .post('/auth/register')
-            .send(TestUser)
+            .send(testUser)
             .expect(201)
             .expect((res) => {
                 expect(res.body).toHaveProperty('id');
-                expect(res.body.email).toBe(TestUser.email);
-                expect(res.body.name).toBe(TestUser.name);
+                expect(res.body.email).toBe(testUser.email);
+                expect(res.body.name).toBe(testUser.name);
                 expect(res.body.password).toBeUndefined();
             });
     });
@@ -39,24 +75,24 @@ describe('AppController (e2e)', () => {
     it('testing registering with duplicated email (POST)', async () => {
         await request(testSetup.app.getHttpServer())
             .post('/auth/register')
-            .send(TestUser);
+            .send(testUser);
 
         return request(testSetup.app.getHttpServer())
             .post('/auth/register')
-            .send(TestUser)
+            .send(testUser)
             .expect(409);
     });
 
     it('testing login (POST)', async () => {
         await request(testSetup.app.getHttpServer())
             .post('/auth/register')
-            .send(TestUser);
+            .send(testUser);
 
         const response = await request(testSetup.app.getHttpServer())
             .post('/auth/login')
             .send({
-                email: TestUser.email,
-                password: TestUser.password,
+                email: testUser.email,
+                password: testUser.password,
             });
 
         expect(response.status).toBe(201);
@@ -66,13 +102,13 @@ describe('AppController (e2e)', () => {
     it('testing Authorization and Token (Get)', async () => {
         await request(testSetup.app.getHttpServer())
             .post('/auth/register')
-            .send(TestUser);
+            .send(testUser);
 
         const response = await request(testSetup.app.getHttpServer())
             .post('/auth/login')
             .send({
-                email: TestUser.email,
-                password: TestUser.password,
+                email: testUser.email,
+                password: testUser.password,
             });
 
         const token = response.body.accessToken;
@@ -82,8 +118,8 @@ describe('AppController (e2e)', () => {
             .set('Authorization', `Bearer ${token}`)
             .expect(200)
             .expect((res) => {
-                expect(res.body.email).toBe(TestUser.email);
-                expect(res.body.name).toBe(TestUser.name);
+                expect(res.body.email).toBe(testUser.email);
+                expect(res.body.name).toBe(testUser.name);
                 expect(res.body.password).toBeUndefined();
             });
     });
@@ -91,13 +127,13 @@ describe('AppController (e2e)', () => {
     it('testing Authorization and Token with fake token(Get)', async () => {
         await request(testSetup.app.getHttpServer())
             .post('/auth/register')
-            .send(TestUser);
+            .send(testUser);
 
         const response = await request(testSetup.app.getHttpServer())
             .post('/auth/login')
             .send({
-                email: TestUser.email,
-                password: TestUser.password,
+                email: testUser.email,
+                password: testUser.password,
             });
 
         const token = response.body.accessToken;

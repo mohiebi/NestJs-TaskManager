729ee97d7e386972e3cf8f9824f2ed21a8e11465
Mohammad Hosein <e.mohamadhosein@gmail.com>
2025-07-04 18:21:37 +0330

    adding end to end test for athentication with token

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -62,4 +62,52 @@ describe('AppController (e2e)', () => {
         expect(response.status).toBe(201);
         expect(response.body.accessToken).toBeDefined();
     });
+
+    it('testing Authorization and Token (Get)', async () => {
+        await request(testSetup.app.getHttpServer())
+            .post('/auth/register')
+            .send(TestUser);
+
+        const response = await request(testSetup.app.getHttpServer())
+            .post('/auth/login')
+            .send({
+                email: TestUser.email,
+                password: TestUser.password,
+            });
+
+        const token = response.body.accessToken;
+
+        return await request(testSetup.app.getHttpServer())
+            .get('/auth/profile')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.email).toBe(TestUser.email);
+                expect(res.body.name).toBe(TestUser.name);
+                expect(res.body.password).toBeUndefined();
+            });
+    });
+
+    it('testing Authorization and Token with fake token(Get)', async () => {
+        await request(testSetup.app.getHttpServer())
+            .post('/auth/register')
+            .send(TestUser);
+
+        const response = await request(testSetup.app.getHttpServer())
+            .post('/auth/login')
+            .send({
+                email: TestUser.email,
+                password: TestUser.password,
+            });
+
+        const token = response.body.accessToken;
+
+        return await request(testSetup.app.getHttpServer())
+            .get('/auth/profile')
+            .set('Authorization', `Bearer xxx`)
+            .expect(401)
+            .expect((res) => {
+                expect(res.body.message).toBe('Unauthorized');
+            });
+    });
 });
